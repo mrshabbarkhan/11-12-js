3e8b0c308f1fdf697ae69e754188d7cd614bfd43
shabbar khan <shabbarkhan.js@gmail.com>
2025-02-21 11:47:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,147 @@
+// console.log(2 + 2, "answer");
+
+// types of variables
+// 1)  var
+// 2) let
+// 3) const
+
+// var bottle = "paniiiii"
+// console.log(bottle)
+// bottle = "khatam"   // var can be updated
+// console.log(bottle)
+// var bottle = "milk"   // var can be reinnitialized
+// console.log(bottle)
+
+// let age = 30
+// age = 40 // let can be updated
+// // let age // let can not be reinnitialized
+// console.log(age)
+
+// const name = "shabbar"
+// console.log(name)
+// name = "harry" // const can not be updated
+// const name  = "shabbar"  // const can not be reinnitialized
+
+// Types of Data
+// 1) Primitive
+// 2) Non Primitive
+
+// Primitive >
+
+// 1) String   --   "",'
+// 2) Number  ---   1234567890
+// 3) Boolean   ------ true, false
+// 4) Null       ------ null
+// 5) Undefined    --------
+// Non primitive
+
+// 1)  Array
+// 2) Object
+// 3) Function
+
+// String
+// let name = "farhan";
+// let age = 24;
+// let isMarried = false;
+// let skills = null
+// console.log(name, age, isMarried, skills);
+
+// let value = 23;
+// console.log(value)
+
+// non Primitive
+
+// Array
+
+let arr = ["hello", 30, true, null];
+2;
+
+// console.log(arr[3]);
+
+let farhan = {
+  name: "farhan",
+  lastName: "Akhatar",
+  address: "Indore",
+};
+let shivani = {
+  name: "shivani",
+  lastName: "Alawa",
+  address: "Indore",
+};
+let shadab = {
+  name: "shadab",
+  lastName: "khan",
+  address: "Indore",
+};
+
+// let students = [
+//   {
+//     name: "shadab",
+//     lastName: "khan",
+//     address: "Indore",
+//   },
+//   {
+//     name: "shivani",
+//     lastName: "Alawa",
+//     address: "Indore",
+//   },
+//   {
+//     name: "farhan",
+//     lastName: "Akhatar",
+//     address: "Indore",
+//   },
+// ];
+
+// console.log(typeof [])
+
+// Operators
+
+// 1) Arithm...    +,-,*,/,%, ++,--
+// 4) comparison   < ,> == , ===, <= , >= 1, !==
+// 2) logical     && , || , !
+// 3) assignment  = ,+= ,-+
+// 5) Ternary    ? , :
+// 6) Bitwise    & ,
+// 7) Type   typeof
+
+// console.log(3 + 4);
+// console.log(45 - 24);
+// console.log(47 / 5);
+// console.log(12 % 5);
+// console.log("10" + 5);
+// console.log(20 + "5");
+
+// // console.log(10 + +"5");
+// // console.log(50 / 5 + 4 - 30 + 40);
+
+// console.log(3 === "3");
+
+// let value  = 30
+
+// if (value >= 10) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let val1 = 200;
+// let val2 = 30;
+// let val3 = 300;
+
+// if (val1 == 200 && val2 == 30 && val3 == 300) {
+//   console.log("Exist");
+// } else {
+//   console.log("Nope");
+// }
+
+// let number = 30;
+
+// if (number === 10) {
+//   console.log("number is = 10");
+// } else if (number === 20) {
+//   console.log("number is = 20");
+// } else {
+//   console.log("Mujhe nhi pata");
+// }
+
+
